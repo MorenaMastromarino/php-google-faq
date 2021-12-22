ab64dcba3f2ff5edebd539a9a7569c541af11d68
MorenaMastromarino <morena.mastromarino98@gmail.com>
2021-12-22 15:07:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+  // Ci sono diverse domande con relative risposte.
+  // Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+  // Consegna minima:
+  // Stampare tutti i paragrafi suddividendo titolo e contenuto
+  // Una volta risolto questo requisito base potete migliorare il lavoro con:
+  // - creare uno spazio fra un sottoparagrafo e l’altro
+  // - gestire il paragrafo “Perché il mio account è associato a un paese?” con gli elenchi
+  // - gestire i link interni al testo
+  // Preoccupatevi della fedeltà grafica solo una volta risolti i problemi PHP
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Google FAQ</title>
+</head>
+<body>
+
+  
+  
+</body>
+</html>
